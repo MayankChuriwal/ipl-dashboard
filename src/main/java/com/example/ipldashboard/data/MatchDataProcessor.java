38988bb4dbe6486012c14d7f12cab9b71f581a07
Mayank Churiwal <churiwalmayank@gmail.com>
2021-11-01 22:16:10 +0530

    Spring batch to read csv and add to embedded DB

---
+++ src/main/java/com/example/ipldashboard/data/MatchDataProcessor.java
@@ -0,0 +1,38 @@
+package com.example.ipldashboard.data;
+
+import java.time.LocalDate;
+
+import com.example.ipldashboard.model.Match;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
+    
+    private static final Logger log = LoggerFactory.getLogger(MatchDataProcessor.class);
+    
+    @Override
+    public Match process(final MatchInput matchInput) throws Exception {
+        
+        Match match = new Match();
+        match.setId(Long.parseLong(matchInput.getId()));
+        match.setCity(matchInput.getCity());
+        match.setDate(LocalDate.parse(matchInput.getDate()));
+        match.setPlayerOfMatch(matchInput.getPlayer_of_match());
+        match.setVenue(matchInput.getVenue());
+        match.setTeam1(matchInput.getTeam1());
+        match.setTeam2(matchInput.getTeam2());
+        match.setTossDecision(matchInput.getToss_decision());
+        match.setTossWinner(matchInput.getToss_winner());
+        match.setResult(matchInput.getResult());
+        match.setMatchWinner(matchInput.getMatchWinner());
+        match.setResultMargin(matchInput.getResult_margin());
+        match.setUmpire1(matchInput.getUmpire1());
+        match.setUmpire2(matchInput.getUmpire2());
+
+        return match;
+  }
+
+}
