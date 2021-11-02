df4a9855dc6aecb51db44f12ed76d46468a62c18
Mayank Churiwal <churiwalmayank@gmail.com>
2021-11-03 01:50:04 +0530

    Get mapping to get data for teams

--- src/main/java/com/example/ipldashboard/model/Team.java
+++ src/main/java/com/example/ipldashboard/model/Team.java
@@ -1,9 +1,12 @@
 package com.example.ipldashboard.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 @Entity
 public class Team {
@@ -11,9 +14,17 @@ public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    
     private String teamName;
     private long totalMatches;
     private long totalWins;
+
+    @Transient
+    private List<Match> matches;
+
+    public Team() {
+
+    }
 
     public Team(String teamName, long totalMatches) {
         this.teamName = teamName;
@@ -43,6 +54,14 @@ public class Team {
     }
     public void setTotalWins(long totalWins) {
         this.totalWins = totalWins;
+    }
+
+    public List<Match> getMatches() {
+        return matches;
+    }
+
+    public void setMatches(List<Match> matches) {
+        this.matches = matches;
     }
 
     @Override
